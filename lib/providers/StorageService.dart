02389cc00066a049d4eaa0bcd555b00a0aa7c19c
Eman Khadim <64742318+emankhadim@users.noreply.github.com>
2022-01-28 17:15:06 +0500

    first commit

---
+++ lib/providers/StorageService.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<String> uploadImage(File image) async {
+    String imgUrl;
+    Reference ref =
+    storage.ref().child("UserProfiles/image" + DateTime.now().toString());
+    UploadTask uploadTask = ref.putFile(image);
+    await uploadTask.whenComplete(() async {
+      String imgUrll = (await ref.getDownloadURL()).toString();
+      imgUrl.value = imgUrll;
+    });
+    return imgUrl.value!;
+  }
+}
